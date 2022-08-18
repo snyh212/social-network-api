a01cd945d2cae51654907c9d94d0842bb2f01e1b
Shmuel Hoffman <snyh212@gmail.com>
2022-08-18 18:37:46 -0400

    pseudocode for user routes

---
+++ Routes/api/index.js
@@ -0,0 +1,1 @@
+// require express, thouts and user routes
---
+++ Routes/api/user-routes.js
@@ -0,0 +1,6 @@
+// require express and user model
+// Get all Users
+// Get 1 user by _id
+// Post a new User
+// Update with put
+// delete user by _id
