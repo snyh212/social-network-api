5ab25de9217e9af8faf372bec8f7f0ffa12484df
Shmuel Hoffman <snyh212@gmail.com>
2022-08-18 23:21:00 -0400

    added index js routes config

--- Routes/api/user-routes.js
+++ Routes/api/user-routes.js
@@ -1,5 +1,18 @@
 // require express and user model
+const app = require('express').Router();
+const { User } = require('../../models')
+
 // Get all Users
+app.get('/', (req, res) => {
+    User.find()
+        .then(userData => {
+            console.log(userData)
+            res.json(userData);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 // Get 1 user by _id
 // Post a new User
 // Update with put
