650536193d623dec719d6e182d6c9a89b1030bb4
Shmuel Hoffman <snyh212@gmail.com>
2022-08-18 23:31:05 -0400

    added user routes

--- Routes/api/user-routes.js
+++ Routes/api/user-routes.js
@@ -40,4 +40,71 @@ app.post('/', ({ body }, res) => {
         });
 });
 // Update with put
+app.put('/:id', ({ params, body }, res) => {
+    User.findOneAndUpdate({ _id: params.id }, body, { runValidators: true, new: true })
+    .then(userData => {
+        if (!userData) {
+            return res.status(404).json({ message: 'No user with this id!' });
+        }
+        res.json(userData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
+});
 // delete user by _id
+app.delete('/:id', ({ params }, res) => {
+    User.findOneAndDelete({ _id: params.id }).then(userData => {
+        if (!userData) {
+            return res.status(404).json({ message: 'No user with this id!' });
+        }
+        res.json(userData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// post and delete for friends
+app.post('/:id/friends/:friendId', ({ params }, res) => {
+    User.findOneAndUpdate(
+        { _id: params.id },
+        { $push: { friends: params.friendId } },
+        { new: true }
+      )
+      .select('-__v')
+      .then(userData => {
+        if (!userData) {
+          res.status(400).json({ message: 'No user found with this id!'});
+          return;
+        }
+        res.json(userData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+});
+
+app.delete('/:id/friends/:friendId', ({ params }, res) => {
+    User.findOneAndUpdate(
+        { _id: params.id },
+        { $pull: { friends: params.friendId } },
+        { new: true }
+      )
+      .select('-__v')
+      .then(userData => {
+        if (!userData) {
+          res.status(400).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(userData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+}); 
