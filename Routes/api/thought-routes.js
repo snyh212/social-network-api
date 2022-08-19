dfb3a55f1cd52db83cae42e6fb0bd9e9bd035e9a
Shmuel Hoffman <snyh212@gmail.com>
2022-08-18 23:41:06 -0400

    added thought get routes for all and 1

--- Routes/api/thought-routes.js
+++ Routes/api/thought-routes.js
@@ -25,6 +25,78 @@ app.get('/:id', ({ params }, res) => {
     })
 });
 // post for new
+app.post('/', ({ body }, res) => {
+    Thought.create(body)
+        .then(({ _id }) =>
+            User.findOneAndUpdate({}, { $push: { thoughts: _id } }, { new: true })
+        )
+        .then(thoughtData => {
+            res.json(thoughtData);
+        })
+});
 // put to update
+app.put('/:id', ({ params, body }, res) => {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { runValidators: true, new: true })
+        .then(thoughtData => {
+            if (!thoughtData) {
+                return res.status(404).json({ message: 'No thought with this id!' });
+            }
+            res.json(thoughtData);
+        })
+});
 // delete
-//api/thoughts/:thoughtId/reactions
+app.delete('/:userId/:thoughtId', ({ params }, res) => {
+    Thought.findOneAndDelete(
+        { _id: params.thoughtId },
+        { new: true, runValidators: true })
+        .then((thoughtData) => {
+          if (!thoughtData) {
+            res.status(404).json({
+              message: "No user found with this id!",
+            });
+            User.findOneAndUpdate(
+              { thoughts: params.thoughtId },
+              { $pull: { thoughts: params.thoughtId } },
+              { new: true }
+            )
+          }
+          
+        }).then(res.json("Thought deleted!"))
+});
+
+//api/thoughts/:thoughtId/reactions post and delete
+app.post('/:id', ({ params, body }, res) => {
+    Thought.findOneAndUpdate
+    (
+      { _id: params.id },
+      { $push: { reactions: body } },
+      { new: true }
+    )
+    .select('-__v')
+    .then(thoughtData => {
+      if (!thoughtData) {
+        res.status(400).json({ message: 'No thought found with this id!' });
+        return;
+      }
+      res.json(thoughtData);
+    })
+});
+
+app.delete('/:userId/:thoughtId/:reactionId', ({ params }, res) => {
+    Thought.findOneAndUpdate(
+        { _id: params.thoughtId},
+        { $pull: { reactions: { reactionId: params.reaction}}},
+        { new: true }
+    )
+    .select('-__v')
+    .then(thoughtData => {
+        if (!dbThoughtData) {
+            res.status(400).json({ message: 'No reaction or thought found with given ids!'
+        });
+        return;
+    }
+        res.json(thoughtData)
+    })
+});
+
+module.exports = app
