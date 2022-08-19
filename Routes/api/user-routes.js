af4f369adcb22955512cdafaa2a26d34ecb5de54
Shmuel Hoffman <snyh212@gmail.com>
2022-08-18 23:21:49 -0400

    added user get all and get 1 routes

--- Routes/api/user-routes.js
+++ Routes/api/user-routes.js
@@ -14,6 +14,21 @@ app.get('/', (req, res) => {
         });
 });
 // Get 1 user by _id
+app.get('/:id', ({ params }, res) => {
+    User.findOne({ _id: params.id })
+    .select('-__v')
+    .then(userData => {
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with this id!'});
+            return;
+        }
+        res.json(userData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err)
+    })
+});
 // Post a new User
 // Update with put
 // delete user by _id
