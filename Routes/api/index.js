4ea0ab4c483c24ad69db4398371c6cef158279a8
Shmuel Hoffman <snyh212@gmail.com>
2022-08-18 23:17:16 -0400

    added index js routes config

--- Routes/api/index.js
+++ Routes/api/index.js
@@ -1,1 +1,9 @@
 // require express, thouts and user routes
+const router = require('express').Router();
+const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./user-routes');
+
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
